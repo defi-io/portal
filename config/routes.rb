d62783a617483019f08de61cc8da73f1bfa13067
Robert <7777@hey.com>
2024-04-04 14:03:05 -0400

    Build a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'dex/jup'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "dex#jup", as: 'home'
+  root "home#index", as: 'home'
 end
