1348d1d608f62706eeb6023d52d25e7b3a6f6f4a
Robert <7777@hey.com>
2024-05-12 18:59:29 -0400

    Integration with spinacms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def title(title = nil)
+    return "DeFi Insights: Crypto News, Trends | DeFi.io" if current_page.title == 'Homepage'
+    current_page.title
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index", as: 'home'
+  mount Spina::Engine => '/feed'
 end
