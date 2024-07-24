a5147e9dbe747ddac5c626c9e97fa3b5cf409177
Robert <7777@hey.com>
2024-07-24 10:52:32 -0400

    Remove log info

--- app/models/tool/cmc.rb
+++ app/models/tool/cmc.rb
@@ -5,14 +5,12 @@ module Tool::Cmc
     doc = get_doc(url)
     symbols = []
     doc.css('table.cmc-table > tbody > tr p.coin-item-symbol').each do |coin|
-      p '=' * 77
-      p symbol = coin.text
+      symbol = coin.text
       symbols << symbol
     end
     
     doc.css('table.cmc-table > tbody > tr span.crypto-symbol').each do |coin|
-      p '*' * 77
-      p symbol = coin.text
+      symbol = coin.text
       symbols << symbol
     end
     
@@ -31,13 +29,10 @@ module Tool::Cmc
     url = "https://coinmarketcap.com/?page=#{page}"
     doc = get_doc(url, 13)
     doc.css('table.cmc-table > tbody > tr').each_with_index do |tr, i|
-      p '-' * 55, i
       symbol = tr.at('a.cmc-link .coin-item-symbol')
       symbol = tr.at('a.cmc-link .crypto-symbol') if symbol.nil?
-      p symbol.text
       td = tr.css('td')[3].to_html
       price = td.match(/[\d,]+\.\d+/).to_s.gsub(',','').to_f
-      p price
       next if price == 0
       lp = LatestPrice.find_by_symbol(symbol.text)
       next if lp.nil?
